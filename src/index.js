6be13ecbe15e1b5af93220fa4e3473f147752bdb
UmanshSarabhai <49119948+UmanshSarabhai@users.noreply.github.com>
2022-08-22 15:03:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,64 +10,7 @@ import {
 } from "@appbaseio/reactivesearch";
 import { Row, Button, Col, Card, Switch, Tree, Popover, Affix } from "antd";
 import "antd/dist/antd.css";
-//import createDOMPurify from 'dompurify';
-import ExpandCollapse from "react-expand-collapse";
-
 import "./styles.css";
-
-//const DOMPurify = createDOMPurify(window);
-
-const { TreeNode } = Tree;
-
-const renderAsTree = (res, key = "0") => {
-  if (!res) return null;
-  const iterable = Array.isArray(res) ? res : Object.keys(res);
-  return iterable.map((item, index) => {
-    const type = typeof res[item];
-    if (type === "string" || type === "number") {
-      return (
-        <TreeNode
-          title={
-            <div>
-              <span>{item}:</span>&nbsp;
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(res[item])
-                }}
-              />
-            </div>
-          }
-          key={key + "-" + (index + 1)}
-        />
-      );
-    }
-    const hasObject = res[item] === undefined && typeof item !== "string";
-    const node = hasObject ? item : res[item];
-    return (
-      <TreeNode
-        title={
-          typeof item !== "string"
-            ? "Object"
-            : "" + (node || Array.isArray(res) ? item : item + ": null")
-        }
-        key={key + "-" + (index + 1)}
-      >
-        {renderAsTree(node, key + "-" + (index + 1))}
-      </TreeNode>
-    );
-  });
-};
-
-function renderItem(res, triggerClickAnalytics) {
-  return (
-    <div onClick={triggerClickAnalytics} className="list-item" key={res._id}>
-      <ExpandCollapse previewHeight="390px" expandText="Show more">
-        {<Tree showLine>{renderAsTree(res)}</Tree>}
-      </ExpandCollapse>
-    </div>
-  );
-}
-
 const App = () => (
   <ReactiveBase
     app="dev"
